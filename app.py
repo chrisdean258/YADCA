84b5c0ec773c78d8aafa6526c80d5e5681ca21b7
Chris Dean <chrisdean258@gmail.com>
2018-04-07 19:17:46 -0400

    simple server

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import render_template
 from flask import request
 from flask import url_for
 from flask import redirect
+from flask import send_from_directory
+import os
 
 app=Flask(__name__)
 
@@ -11,3 +13,9 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    print(root_dir)
+    return send_from_directory(os.path.join(root_dir,'YADCA' ,'static'), filename)
