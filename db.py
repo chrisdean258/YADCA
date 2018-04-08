d1e1e41b34c38e61b5deb8526a7bf0ba30a85edc
Chris Dean <chrisdean258@gmail.com>
2018-04-07 23:29:31 -0400

    db like stuff

---
+++ db.py
@@ -0,0 +1,14 @@
+from Game import Game
+db = {}
+
+def save_player(roomid, playername, charname, charclass, charrace):
+    global db
+    db[str(roomid)].addPlayer(playername, charname, charclass, charrace)
+
+def new_game(dm_name):
+    global db
+    roomid = len(db)
+    while str(roomid) in db:
+        roomid += 1
+    db[str(roomid)] = Game(dm_name)
+    return roomid
